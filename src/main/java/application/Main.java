713eb3a4de25bbec177ea3e5e4d5071cf32b09bf
Lucas Lopes <hallz@MacBook-Pro-de-Lucas.local>
2020-05-22 14:36:42 -0300

    Intanciando EntityManagerFactory e EntityManager

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import classes.User;
 
 public class Main {
@@ -10,9 +14,8 @@ public class Main {
 		User u2 = new User(2, "viviane@gmail.com", "4321");
 		User u3 = new User(3, "livia@gmail.com", "1243");
 		
-		System.out.println(u);
-		System.out.println(u2);
-		System.out.println(u3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("entity-jpa");
+		EntityManager em = emf.createEntityManager();
 
 	}
 
