a25fe2b72e51655a1a2bc3bafaf63b06edfb0ee7
Lucas Lopes <hallz@MacBook-Pro-de-Lucas.local>
2020-05-22 14:27:32 -0300

    mapeamento da class @Entity

--- src/main/java/classes/User.java
+++ src/main/java/classes/User.java
@@ -1,7 +1,15 @@
 package classes;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer Id;
 	private String email;
 	private String password;
